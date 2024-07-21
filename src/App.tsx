491b7357ad808ae1237ed74124a09808725a12ad
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2024-07-21 22:53:23 +0700

    code format

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
+
+import reactLogo from "./assets/react.svg";
+
 import "./App.css";
 
 function App() {
