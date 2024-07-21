74104d0e04086539fede5be38106ca7473de1ccd
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2024-07-21 23:54:35 +0700

    moved to top bar component file

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,4 @@
-import { BookOpenText, Dot, FileText, FolderOpen, Save } from "lucide-react";
-
-import { Button } from "~/components/ui/button";
+import TopBar from "~/components/top-bar";
 import {
   ResizableHandle,
   ResizablePanel,
@@ -10,27 +8,7 @@ import {
 export default function App() {
   return (
     <>
-      <div className="flex h-8 w-full select-none items-center justify-between border-b px-1">
-        <div className="flex items-center">
-          <FileText className="mx-1" size={14} />
-          <p className="text-xs">my-markdown.md</p>
-          <Dot size={28} className="-ml-1.5" />
-        </div>
-        <div className="flex items-center space-x-1">
-          <Button size="iconSm" variant="ghost">
-            <BookOpenText size={14} />
-            <span className="sr-only">Toggle preview</span>
-          </Button>
-          <Button size="iconSm" variant="ghost">
-            <FolderOpen size={14} />
-            <span className="sr-only">Open</span>
-          </Button>
-          <Button size="iconSm" variant="ghost">
-            <Save size={14} />
-            <span className="sr-only">Save</span>
-          </Button>
-        </div>
-      </div>
+      <TopBar />
       <div className="h-[calc(100vh-2rem)] w-full">
         <ResizablePanelGroup direction="horizontal">
           <ResizablePanel>Editor</ResizablePanel>
---
+++ src/components/top-bar.tsx
@@ -0,0 +1,29 @@
+import { BookOpenText, Dot, FileText, FolderOpen, Save } from "lucide-react";
+
+import { Button } from "~/components/ui/button";
+
+export default function TopBar() {
+  return (
+    <div className="flex h-8 w-full select-none items-center justify-between border-b px-1">
+      <div className="flex items-center">
+        <FileText className="mx-1" size={14} />
+        <p className="text-xs">my-markdown.md</p>
+        <Dot size={28} className="-ml-1.5" />
+      </div>
+      <div className="flex items-center space-x-1">
+        <Button size="iconSm" variant="ghost">
+          <BookOpenText size={14} />
+          <span className="sr-only">Toggle preview</span>
+        </Button>
+        <Button size="iconSm" variant="ghost">
+          <FolderOpen size={14} />
+          <span className="sr-only">Open</span>
+        </Button>
+        <Button size="iconSm" variant="ghost">
+          <Save size={14} />
+          <span className="sr-only">Save</span>
+        </Button>
+      </div>
+    </div>
+  );
+}
