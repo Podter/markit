510ffa87fc284c79b3748200fd51440a0289ad5e
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2024-07-21 23:10:40 +0700

    added shadcn ui

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,20 @@
+import path from "path";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
   plugins: [react()],
-
-  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-  //
-  // 1. prevent vite from obscuring rust errors
+  resolve: {
+    alias: {
+      "~": path.resolve(__dirname, "./src"),
+    },
+  },
   clearScreen: false,
-  // 2. tauri expects a fixed port, fail if that port is not available
   server: {
     port: 1420,
     strictPort: true,
     watch: {
-      // 3. tell vite to ignore watching `src-tauri`
       ignored: ["**/src-tauri/**"],
     },
   },
