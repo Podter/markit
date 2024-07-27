07e0805cd306313a0feeccbeb2ea9564135e7bd8
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2024-07-28 00:39:23 +0700

    added million

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import path from "path";
 import react from "@vitejs/plugin-react-swc";
+import million from "million/compiler";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [react()],
+  plugins: [million.vite({ auto: true }), react()],
   resolve: {
     alias: {
       "~": path.resolve(__dirname, "./src"),
